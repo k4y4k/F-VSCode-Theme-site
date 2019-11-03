eb28423e51b2ca4fde68879e48b45af1f4ea3706
kayak <41324271+by-k4y4k@users.noreply.github.com>
2019-11-03 21:35:23 +1000

    chore(misc): get shipshape for experimenting

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,9 @@
-import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
-import { css, jsx } from '@emotion/core'
-import styled from '@emotion/styled'
-
-const DownloadButton = styled.button`
-  background: none;
-  color: teal;
-  border: none;
-`
-
-const FlexSpacer = styled.div`
-  flex-grow: 1;
-`
 
 const Header = ({ siteTitle }) => (
-  <div
-    css={css`
-      padding: 32px;
-      font-size: 24px;
-      background: linear-gradient(to bottom, #2a2021, #231b1b);
-      color: white;
-      display: flex;
-    `}
-  >
+  <div>
     <h1>{siteTitle}</h1>
-    <FlexSpacer></FlexSpacer>
-    <DownloadButton>Download</DownloadButton>
   </div>
 )
 
